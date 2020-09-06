7123b275622b59073eaa039723c1dbab099071c8
Nikolaos Dimopoulos <nikos@niden.net>
2020-09-06 17:39:29 -0400

    [#27] - Stream adapter for Logger

---
+++ tests/unit/Logger/Adapter/Stream/AddCest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Logger\Adapter\Stream;
+
+use Phalcon\Logger;
+use Phalcon\Logger\Adapter\Stream;
+use Phalcon\Logger\Item;
+use UnitTester;
+
+class AddCest
+{
+    /**
+     * Tests Phalcon\Logger\Adapter\Stream :: add()
+     */
+    public function loggerAdapterStreamAdd(UnitTester $I)
+    {
+        $I->wantToTest('Logger\Adapter\Stream - add()');
+        $fileName   = $I->getNewFileName('log', 'log');
+        $outputPath = logsDir();
+        $adapter    = new Stream($outputPath . $fileName);
+
+        $adapter->begin();
+        $item1 = new Item('Message 1', 'debug', Logger::DEBUG);
+        $item2 = new Item('Message 2', 'debug', Logger::DEBUG);
+        $item3 = new Item('Message 3', 'debug', Logger::DEBUG);
+
+        $adapter
+            ->add($item1)
+            ->add($item2)
+            ->add($item3)
+        ;
+
+        $I->amInPath($outputPath);
+        $I->dontSeeFileFound($fileName);
+
+        $adapter->commit();
+
+        $I->amInPath($outputPath);
+        $I->seeFileFound($fileName);
+        $I->openFile($fileName);
+        $I->seeInThisFile('Message 1');
+        $I->seeInThisFile('Message 2');
+        $I->seeInThisFile('Message 3');
+
+        $adapter->close();
+        $I->safeDeleteFile($outputPath . $fileName);
+    }
+}
---
+++ tests/unit/Logger/Adapter/Stream/ProcessCest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Logger\Adapter\Stream;
+
+use Codeception\Stub;
+use LogicException;
+use Phalcon\Logger;
+use Phalcon\Logger\Adapter\Stream;
+use Phalcon\Logger\Exception;
+use Phalcon\Logger\Item;
+use UnitTester;
+
+use function logsDir;
+
+class ProcessCest
+{
+    /**
+     * Tests Phalcon\Logger\Adapter\Stream :: process()
+     *
+     * @throws Exception
+     */
+    public function loggerAdapterStreamProcess(UnitTester $I)
+    {
+        $I->wantToTest('Logger\Adapter\Stream - process()');
+        $fileName   = $I->getNewFileName('log', 'log');
+        $outputPath = logsDir();
+        $adapter    = new Stream($outputPath . $fileName);
+
+        $item = new Item('Message 1', 'debug', Logger::DEBUG);
+        $adapter->process($item);
+
+        $I->amInPath($outputPath);
+        $I->seeFileFound($fileName);
+        $I->openFile($fileName);
+        $I->seeInThisFile('Message 1');
+
+        $adapter->close();
+        $I->safeDeleteFile($outputPath . $fileName);
+    }
+
+    /**
+     * Tests Phalcon\Logger\Adapter\Stream :: process() - exception
+     *
+     * @throws Exception
+     */
+    public function loggerAdapterStreamProcessException(UnitTester $I)
+    {
+        $I->wantToTest('Logger\Adapter\Stream - process() - exception');
+
+        $fileName    = $I->getNewFileName('log', 'log');
+        $outputPath  = logsDir();
+
+        $I->expectThrowable(
+            new LogicException(
+                "The file '" . $outputPath . $fileName
+                . "' cannot be opened with mode 'ab'"
+            ),
+            function () use ($outputPath, $fileName) {
+                $adapter = Stub::construct(
+                    Stream::class,
+                    [
+                        $outputPath . $fileName
+                    ],
+                    [
+                        'fopen' => false,
+                    ]
+                );
+
+                $item = new Item('Message 1', 'debug', Logger::DEBUG);
+                $adapter->process($item);
+            }
+        );
+
+        $I->safeDeleteFile($outputPath . $fileName);
+    }
+}
