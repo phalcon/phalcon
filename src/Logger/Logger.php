dd230a3d58a80e5079eceed7c59cd2208d0bca80
Nikolaos Dimopoulos <nikos@niden.net>
2020-09-07 17:33:29 -0400

    [#27] - Adding codacy.yml file

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -19,7 +19,9 @@ use Exception;
 use Phalcon\Logger\Adapter\AdapterInterface;
 use Phalcon\Logger\Exception as LoggerException;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
 
+use function array_diff_key;
 use function array_flip;
 use function date_default_timezone_get;
 use function is_numeric;
@@ -38,6 +40,8 @@ use function is_string;
  */
 class Logger implements LoggerInterface
 {
+    use LoggerTrait;
+
     public const ALERT     = 2;
     public const CRITICAL  = 1;
     public const CUSTOM    = 8;
@@ -119,77 +123,6 @@ class Logger implements LoggerInterface
     }
 
     /**
-     * Action must be taken immediately.
-     *
-     * Example: Entire website down, database unavailable, etc. This should
-     * trigger the SMS alerts and wake you up.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @throws LoggerException
-     */
-    public function alert($message, array $context = []): void
-    {
-        $this->addMessage(self::ALERT, (string) $message, $context);
-    }
-
-    /**
-     * Critical conditions.
-     *
-     * Example: Application component unavailable, unexpected exception.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @throws LoggerException
-     */
-    public function critical($message, array $context = []): void
-    {
-        $this->addMessage(self::CRITICAL, (string) $message, $context);
-    }
-
-    /**
-     * Detailed debug information.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @throws LoggerException
-     */
-    public function debug($message, array $context = []): void
-    {
-        $this->addMessage(self::DEBUG, (string) $message, $context);
-    }
-
-    /**
-     * Runtime errors that do not require immediate action but should typically
-     * be logged and monitored.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @throws LoggerException
-     */
-    public function error($message, array $context = []): void
-    {
-        $this->addMessage(self::ERROR, (string) $message, $context);
-    }
-
-    /**
-     * System is unusable.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @throws LoggerException
-     */
-    public function emergency($message, array $context = []): void
-    {
-        $this->addMessage(self::EMERGENCY, (string) $message, $context);
-    }
-
-    /**
      * Exclude certain adapters.
      *
      * @param array $adapters
@@ -263,21 +196,6 @@ class Logger implements LoggerInterface
     }
 
     /**
-     * Interesting events.
-     *
-     * Example: User logs in, SQL logs.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @throws LoggerException
-     */
-    public function info($message, array $context = []): void
-    {
-        $this->addMessage(self::INFO, (string) $message, $context);
-    }
-
-    /**
      * Logs with an arbitrary level.
      *
      * @param mixed  $level
@@ -291,19 +209,6 @@ class Logger implements LoggerInterface
         $intLevel = $this->getLevelNumber($level);
 
         $this->addMessage($intLevel, (string) $message, $context);
-    }
-
-    /**
-     * Normal but significant events.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @throws LoggerException
-     */
-    public function notice($message, array $context = []): void
-    {
-        $this->addMessage(self::NOTICE, (string) $message, $context);
     }
 
     /**
@@ -355,22 +260,6 @@ class Logger implements LoggerInterface
     }
 
     /**
-     * LoggerExceptional occurrences that are not errors.
-     *
-     * Example: Use of deprecated APIs, poor use of an API, undesirable things
-     * that are not necessarily wrong.
-     *
-     * @param string $message
-     * @param array  $context
-     *
-     * @throws LoggerException
-     */
-    public function warning($message, array $context = []): void
-    {
-        $this->addMessage(self::WARNING, (string) $message, $context);
-    }
-
-    /**
      * Adds a message to each handler for processing
      *
      * @param int    $level
@@ -405,13 +294,12 @@ class Logger implements LoggerInterface
             /**
              * Log only if the key does not exist in the excluded ones
              */
-            foreach ($this->adapters as $name => $adapter) {
-                if (!isset($this->excluded[$name])) {
-                    if ($adapter->inTransaction()) {
-                        $adapter->add($item);
-                    } else {
-                        $adapter->process($item);
-                    }
+            $adapters = array_diff_key($this->adapters, $this->excluded);
+            foreach ($adapters as $name => $adapter) {
+                if ($adapter->inTransaction()) {
+                    $adapter->add($item);
+                } else {
+                    $adapter->process($item);
                 }
             }
 
