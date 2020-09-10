7d9b9c6cf359215cd938522383759869d974e759
Nikolaos Dimopoulos <nikos@niden.net>
2020-09-09 20:01:07 -0400

    feat: supporting traits to split the collection by interface
    Refs #7

---
+++ src/Collection/Traits/ArrayAccessTrait.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Collection\Traits;
+
+trait ArrayAccessTrait
+{
+    /**
+     * Get the element from the collection
+     *
+     * @param string      $element
+     * @param mixed|null  $defaultValue
+     * @param string|null $cast
+     *
+     * @return mixed
+     */
+    abstract public function get(
+        string $element,
+        $defaultValue = null,
+        string $cast = null
+    );
+
+    /**
+     * Get the element from the collection
+     *
+     * @param string $element
+     *
+     * @return bool
+     */
+    abstract public function has(string $element): bool;
+
+    /**
+     * Whether a offset exists
+     *
+     * @link https://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $element
+     *
+     * @return bool
+     */
+    public function offsetExists($element): bool
+    {
+        $element = (string) $element;
+
+        return $this->has($element);
+    }
+
+    /**
+     * Offset to retrieve
+     *
+     * @link https://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $element
+     *
+     * @return mixed
+     */
+    public function offsetGet($element)
+    {
+        $element = (string) $element;
+
+        return $this->get($element);
+    }
+
+    /**
+     * Offset to set
+     *
+     * @link https://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $element
+     * @param mixed $value
+     */
+    public function offsetSet($element, $value): void
+    {
+        $element = (string) $element;
+
+        $this->set($element, $value);
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @link https://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $element
+     */
+    public function offsetUnset($element): void
+    {
+        $element = (string) $element;
+
+        $this->remove($element);
+    }
+
+    /**
+     * Delete the element from the collection
+     *
+     * @param string $element
+     */
+    abstract public function remove(string $element): void;
+
+    /**
+     * Set an element in the collection
+     *
+     * @param string $element
+     * @param mixed  $value
+     */
+    abstract public function set(string $element, $value): void;
+}
---
+++ src/Collection/Traits/SerializableTrait.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Collection\Traits;
+
+use function serialize;
+use function unserialize;
+
+trait SerializableTrait
+{
+    abstract public function init(array $data = []): void;
+
+    /**
+     * String representation of object
+     *
+     * @link https://php.net/manual/en/serializable.serialize.php
+     */
+    public function serialize(): string
+    {
+        return serialize($this->toArray());
+    }
+
+    /**
+     * Returns the object in an array format
+     */
+    abstract public function toArray(): array;
+
+    /**
+     * Constructs the object
+     *
+     * @link https://php.net/manual/en/serializable.unserialize.php
+     *
+     * @param string $serialized
+     */
+    public function unserialize($serialized)
+    {
+        $serialized = (string) $serialized;
+        $data       = unserialize($serialized);
+
+        $this->init($data);
+    }
+}
