9976224b42e69c2520930289c105341f7c1c3286
Nikolaos Dimopoulos <nikos@niden.net>
2020-09-09 20:07:07 -0400

    feat: added readonly class for the collection
    Refs #7

---
+++ src/Collection/ReadOnly.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Collection;
+
+use Phalcon\Collection\Collection;
+
+/**
+ * Phalcon\Collection\ReadOnly is a read only Collection object
+ */
+class ReadOnly extends Collection
+{
+    /**
+     * Delete the element from the collection
+     *
+     * @param string $element
+     *
+     * @throws Exception
+     */
+    public function remove(string $element): void
+    {
+        throw new Exception('The object is read only');
+    }
+
+    /**
+     * Set an element in the collection
+     *
+     * @param string $element
+     * @param mixed  $value
+     *
+     * @throws Exception
+     */
+    public function set(string $element, $value): void
+    {
+        throw new Exception('The object is read only');
+    }
+}
