10109bc089d717add71593450d9c8b83dfd8d1cd
Nikolaos Dimopoulos <nikos@niden.net>
2020-10-06 18:13:24 -0400

    feat(session): added the manager and manager interface for sessions
    Refs #33

--- src/Session/ManagerInterface.php
+++ src/Session/ManagerInterface.php
@@ -24,116 +24,176 @@ use SessionHandlerInterface;
  */
 interface ManagerInterface
 {
-    const SESSION_ACTIVE   = 2;
-    const SESSION_DISABLED = 0;
-    const SESSION_NONE     = 1;
+    public const SESSION_ACTIVE   = 2;
+    public const SESSION_DISABLED = 0;
+    public const SESSION_NONE     = 1;
 
     /**
      * Alias: Gets a session variable from an application context
+     *
+     * @param string $key
+     *
+     * @return mixed
      */
-    public function __get(string key): var;
+    public function __get(string $key);
 
     /**
      * Alias: Check whether a session variable is set in an application context
+     *
+     * @param string $key
+     *
+     * @return bool
      */
-    public function __isset(string key): bool;
+    public function __isset(string $key): bool;
 
     /**
      * Alias: Sets a session variable in an application context
+     *
+     * @param string $key
+     * @param mixed  $value
      */
-    public function __set(string key, var value): void;
+    public function __set(string $key, $value): void;
 
     /**
      * Alias: Removes a session variable from an application context
+     *
+     * @param string $key
      */
-    public function __unset(string key): void;
+    public function __unset(string $key): void;
 
     /**
      * Check whether the session has been started
+     *
+     * @return bool
      */
     public function exists(): bool;
 
     /**
      * Destroy/end a session
+     *
      */
     public function destroy(): void;
 
     /**
      * Gets a session variable from an application context
+     *
+     * @param string     $key
+     * @param mixed|null $defaultValue
+     * @param bool       $remove
+     *
+     * @return mixed
      */
-    public function get(string key, var defaultValue = null, bool remove = false): var;
+    public function get(string $key, $defaultValue = null, bool $remove = false);
 
     /**
      * Returns the session id
+     *
+     * @return string
      */
     public function getId(): string;
 
     /**
      * Returns the stored session adapter
+     *
+     * @return SessionHandlerInterface
      */
-    public function getAdapter(): <SessionHandlerInterface>;
+    public function getAdapter(): SessionHandlerInterface;
 
     /**
      * Returns the name of the session
+     *
+     * @return string
      */
     public function getName(): string;
 
     /**
      * Get internal options
+     *
+     * @return array
      */
     public function getOptions(): array;
 
     /**
      * Check whether a session variable is set in an application context
+     *
+     * @param string $key
+     *
+     * @return bool
      */
-    public function has(string key): bool;
+    public function has(string $key): bool;
 
     /**
      * Removes a session variable from an application context
+     *
+     * @param string $key
      */
-    public function remove(string key): void;
+    public function remove(string $key): void;
 
     /**
      * Sets a session variable in an application context
+     *
+     * @param string $key
+     * @param mixed  $value
      */
-    public function set(string key, var value): void;
+    public function set(string $key, $value): void;
 
     /**
      * Set the adapter for the session
+     *
+     * @param SessionHandlerInterface $adapter
+     *
+     * @return ManagerInterface
      */
-    public function setAdapter(<SessionHandlerInterface> adapter): <ManagerInterface>;
+    public function setAdapter(SessionHandlerInterface $adapter): ManagerInterface;
 
     /**
      * Set session Id
+     *
+     * @param string $sessionId
+     *
+     * @return ManagerInterface
      */
-    public function setId(string id): <ManagerInterface>;
+    public function setId(string $sessionId): ManagerInterface;
 
     /**
      * Set the session name. Throw exception if the session has started
      * and do not allow poop names
      *
+     * @param string $name
+     *
+     * @return ManagerInterface
      * @throws InvalidArgumentException
      */
-    public function setName(string name): <ManagerInterface>;
+    public function setName(string $name): ManagerInterface;
 
     /**
      * Sets session's options
+     *
+     * @param array $options
      */
-    public function setOptions(array options): void;
+    public function setOptions(array $options): void;
 
     /**
      * Returns the status of the current session.
+     *
+     * @return int
      */
     public function status(): int;
 
     /**
      * Starts the session (if headers are already sent the session will not be
      * started)
+     *
+     * @return bool
      */
     public function start(): bool;
 
     /**
      * Regenerates the session id using the adapter.
+     *
+     * @param bool $deleteOldSession
+     *
+     * @return ManagerInterface
      */
-    public function regenerateId(deleteOldSession = true): <ManagerInterface>;
+    public function regenerateId(bool $deleteOldSession = true): ManagerInterface;
 }
