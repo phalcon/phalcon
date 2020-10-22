afa5cb9d56f6e608e597cd2dbd7848bab41bbd82
Nikolaos Dimopoulos <nikos@niden.net>
2020-10-22 13:03:54 -0400

    test(cache): added fixtures for cache
    Refs #5

---
+++ tests/_data/fixtures/Cache/Adapter/Libmemcached.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Fixtures\Cache\Adapter;
+
+use Phalcon\Cache\Adapter\Libmemcached as CacheLibmemcached;
+use Phalcon\Storage\Exception;
+
+/**
+ * Phalcon\Test\Fixtures\Cache\Adapter
+ *
+ * Libmemcached adapter fixture
+ */
+class Libmemcached extends CacheLibmemcached
+{
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param \DateInterval|int|null $ttl
+     *
+     * @return int
+     * @throws Exception
+     */
+    public function getTtl($ttl = null): int
+    {
+        return parent::getTtl($ttl);
+    }
+}
