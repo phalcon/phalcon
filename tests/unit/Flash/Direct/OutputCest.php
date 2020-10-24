2b0ea5e6e0c24fe424504e3fdd3512fb5166b2d5
Nikolaos Dimopoulos <nikos@niden.net>
2020-10-23 22:19:20 -0400

    refactor(flash): added the tests in flash
    Refs #20

---
+++ tests/unit/Flash/Direct/OutputCest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Flash\Direct;
+
+use UnitTester;
+
+class OutputCest
+{
+    /**
+     * Tests Phalcon\Flash\Direct :: output()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function flashDirectOutput(UnitTester $I)
+    {
+        $I->wantToTest('Flash\Direct - output()');
+
+        $I->skipTest('Need implementation');
+    }
+}
