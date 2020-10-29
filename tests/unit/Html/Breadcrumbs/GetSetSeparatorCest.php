47bed3662c68feebf7d5722d61aa674730dcef44
Nikolaos Dimopoulos <nikos@niden.net>
2020-10-29 17:02:44 -0400

    test(html-breadcrumbs): added tests relating to breadcrumbs
    Refs #23

---
+++ tests/unit/Html/Breadcrumbs/GetSetSeparatorCest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Html\Breadcrumbs;
+
+use Phalcon\Html\Breadcrumbs;
+use UnitTester;
+
+class GetSetSeparatorCest
+{
+    /**
+     * Tests Phalcon\Html\Breadcrumbs :: getSeparator()/setSeparator()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function htmlBreadcrumbsGetSetSeparator(UnitTester $I)
+    {
+        $I->wantToTest('Html\Breadcrumbs - getSeparator()/setSeparator()');
+
+        $breadcrumbs = new Breadcrumbs();
+
+        $expected = ' -:- ';
+
+        $breadcrumbs->setSeparator($expected);
+
+        $I->assertEquals(
+            $expected,
+            $breadcrumbs->getSeparator()
+        );
+    }
+}
---
+++ tests/unit/Html/Breadcrumbs/ToArrayCest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Html\Breadcrumbs;
+
+use Phalcon\Html\Breadcrumbs;
+use UnitTester;
+
+class ToArrayCest
+{
+    /**
+     * Tests Phalcon\Html\Breadcrumbs :: toArray()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function htmlBreadcrumbsToArray(UnitTester $I)
+    {
+        $I->wantToTest('Html\Breadcrumbs - toArray()');
+
+        $breadcrumbs = new Breadcrumbs();
+
+        $breadcrumbs
+            ->add('Home', '/')
+            ->add('Users', '/users')
+            ->add('Phalcon Team')
+        ;
+
+        $expected = [
+            '/'      => 'Home',
+            '/users' => 'Users',
+            ''       => 'Phalcon Team',
+        ];
+
+        $I->assertEquals(
+            $expected,
+            $breadcrumbs->toArray()
+        );
+    }
+}
