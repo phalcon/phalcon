c63e4c0ef05106f51f3157736adf21169c17b49c
Nikolaos Dimopoulos <nikos@niden.net>
2020-12-07 13:19:07 -0500

    refactor(security): initial work, bringing over tests and code from zephir
    Refs #32

---
+++ src/Security/JWT/Token/Enum.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Security\JWT\Token;
+
+/**
+ * Class Enum
+ *
+ * @link https://tools.ietf.org/html/rfc7519
+ */
+class Enum
+{
+    /**
+     * Headers
+     */
+    public const TYPE         = 'typ';
+    public const ALGO         = 'alg';
+    public const CONTENT_TYPE = 'cty';
+
+    /**
+     * Claims
+     */
+    public const AUDIENCE        = 'aud';
+    public const EXPIRATION_TIME = 'exp';
+    public const ID              = 'jti';
+    public const ISSUED_AT       = 'iat';
+    public const ISSUER          = 'iss';
+    public const NOT_BEFORE      = 'nbf';
+    public const SUBJECT         = 'sub';
+}
