982403041e82ae9e060efca14ea867aa9ca2ac45
Nikolaos Dimopoulos <nikos@niden.net>
2020-12-17 11:46:25 -0500

    refactor(acl): enum and exception refactoring
    Refs #1

---
+++ src/Acl/Enum.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Acl;
+
+/**
+ * Constants for Phalcon\Acl\Adapter adapters
+ */
+class Enum
+{
+    public const ALLOW = 1;
+    public const DENY  = 0;
+}
