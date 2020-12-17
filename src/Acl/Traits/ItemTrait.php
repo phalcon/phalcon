3bf47bb91e0b82aeb84332cc0238a6ff78f86e1a
Nikolaos Dimopoulos <nikos@niden.net>
2020-12-17 12:09:41 -0500

    refactor(acl): translated component, role and abstract adapter
    Refs #1

---
+++ src/Acl/Traits/ItemTrait.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Acl\Traits;
+
+use Phalcon\Acl\Exception;
+
+/**
+ * This class defines role entity and its description
+ *
+ * @package Phalcon\Acl\Traits
+ *
+ * @property string      $name
+ * @property string|null $description
+ */
+trait ItemTrait
+{
+    /**
+     * Role name
+     *
+     * @var string
+     */
+    private string $name;
+
+    /**
+     * Role description
+     *
+     * @var string
+     */
+    private ?string $description = null;
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+}
