2d23d9d64f82971f7cbc127f42900db10c3a2581
Nikolaos Dimopoulos <nikos@niden.net>
2020-12-24 16:29:19 -0500

    style(application): phpcs
    Refs #3

--- tests/unit/Application/GetSetDICest.php
+++ tests/unit/Application/GetSetDICest.php
@@ -13,12 +13,10 @@ declare(strict_types=1);
 
 namespace Phalcon\Tests\Unit\Application;
 
-use Phalcon\Application\AbstractApplication;
 use Phalcon\Di\FactoryDefault;
-use Phalcon\Di\InjectionAwareInterface;
-use Phalcon\Events\EventsAwareInterface;
 use Phalcon\Tests\Fixtures\Application\ApplicationFixture;
 use UnitTester;
+
 use function spl_object_hash;
 
 /**
