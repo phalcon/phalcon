b6531360d8e9208fcde7743284ce5c4e30d2933d
Michael Rynn <michael.rynn.500@gmail.com>
2020-12-29 09:15:16 +1100

    EntityInterface

---
+++ src/Mvc/EntityInterface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Mvc;
+
+/**
+ * Phalcon\Mvc\EntityInterface
+ *
+ * Interface for Phalcon\Mvc\Collection and Phalcon\Mvc\Model
+ */
+interface EntityInterface
+{
+    /**
+     * Reads an attribute value by its name
+     */
+    public function readAttribute(string $attribute): mixed;
+
+    /**
+     * Writes an attribute value by its name
+     */
+    public function writeAttribute(string $attribute, $value): void;
+}
