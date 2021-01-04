09095f64938e2356d49a73915ca3006361d08667
Anton Vasiliev <aionbase@gmail.com>
2021-01-04 21:12:10 +0000

    #9 - Refactor PSR-11 Container class

---
+++ src/Container.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon;
+
+use Psr\Container\ContainerInterface;
+use Phalcon\Di\DiInterface;
+
+/**
+ * PSR-11 Wrapper for `Phalcon\Di`
+ */
+class Container implements ContainerInterface
+{
+    /**
+     * @var DiInterface
+     */
+    protected DiInterface $container;
+
+    /**
+     * Phalcon\Container constructor
+     *
+     * @param DiInterface $container
+     */
+    public function __construct(DiInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Return the service
+     *
+     * @param string $id
+     * @return mixed
+     */
+    public function get($id)
+    {
+        return $this->container->getShared($id);
+    }
+
+    /**
+     * Whether a service exists or not in the container
+     *
+     * @param string $id
+     * @return bool
+     */
+    public function has($id): bool
+    {
+        return $this->container->has($id);
+    }
+}
---
+++ tests/unit/ContainerCest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit;
+
+use Phalcon\Container;
+use Phalcon\Di\Di;
+use Phalcon\Di\Exception;
+use UnitTester;
+
+final class ContainerCest
+{
+    /**
+     * Tests Phalcon\Container :: get
+     *
+     * @param UnitTester $I
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2021-01-04
+     */
+    public function get(UnitTester $I): void
+    {
+        $I->wantToTest('PSR-11 Container - get');
+
+        $di = new Di();
+        $di->setShared('service', function () {
+            return new \stdClass();
+        });
+
+        $container = new Container($di);
+
+        $I->expectThrowable(Exception::class, function () use ($container) {
+            $container->get('empty');
+        });
+
+        $I->assertInstanceOf(\stdClass::class, $container->get('service'));
+    }
+
+    /**
+     * Tests Phalcon\Container :: has
+     *
+     * @param UnitTester $I
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2021-01-04
+     */
+    public function has(UnitTester $I): void
+    {
+        $I->wantToTest('PSR-11 Container - has');
+
+        $di = new Di();
+        $di->setShared('issetService', function () {
+            return true;
+        });
+
+        $container = new Container($di);
+
+        $I->assertFalse($container->has('empty'));
+        $I->assertTrue($container->has('issetService'));
+    }
+}
