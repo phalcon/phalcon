59965a759477362e316e8b9a59507145d1b32337
Jeremy <pastouret.jeremy@gmail.com>
2021-02-13 10:50:32 +0100

    feat(Attributes) - add missing item for forms

---
+++ src/Html/Attributes.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Html;
+
+use Phalcon\Collection;
+use Phalcon\Html\Attributes\RenderInterface;
+use Phalcon\Tag;
+
+/**
+ * This class helps to work with HTML Attributes
+ */
+class Attributes extends Collection implements RenderInterface
+{
+    /**
+     * Render attributes as HTML attributes
+     */
+    public function render():string
+    {
+        return Tag::renderAttributes("", $this->toArray());
+    }
+
+    /**
+     * Alias of the render method
+     */
+    public function __toString():string
+    {
+        return $this->render();
+    }
+}
---
+++ src/Html/Attributes/AttributesInterface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Html\Attributes;
+
+use Phalcon\Html\Attributes;
+
+/**
+* Phalcon\Html\Attributes\AttributesInterface
+*
+* Interface Phalcon\Html\Attributes\AttributesInterface
+*/
+interface AttributesInterface
+{
+    /**
+     * Get Attributes
+     */
+    public function getAttributes():Attributes;
+
+    /**
+     * Set Attributes
+     */
+    public function setAttributes(Attributes $attributes):AttributesInterface;
+}
