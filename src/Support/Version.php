d772f564546e1d6e05a8f4a9194e37e5112eaa37
Nikolaos Dimopoulos <nikos@niden.net>
2021-09-06 11:21:05 -0400

    support(version): moved version under support; added tests
    Refs #6.0.x

---
+++ src/Support/Version.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Support;
+
+/**
+ * This class allows to get the installed version of the framework
+ */
+class Version
+{
+    /**
+     * The constant referencing the major version. Returns 0
+     *
+     * ```php
+     * echo (new Phalcon\Support\Version())
+     *          ->getPart(Phalcon\Support\Version::VERSION_MAJOR);
+     * ```
+     */
+    const VERSION_MAJOR = 0;
+
+    /**
+     * The constant referencing the major version. Returns 1
+     *
+     * ```php
+     * echo (new Phalcon\Support\Version())
+     *          ->getPart(Phalcon\Support\Version::VERSION_MEDIUM);
+     * ```
+     */
+    const VERSION_MEDIUM = 1;
+
+    /**
+     * The constant referencing the major version. Returns 2
+     *
+     * ```php
+     * echo (new Phalcon\Support\Version())
+     *          ->getPart(Phalcon\Support\Version::VERSION_MINOR);
+     * ```
+     */
+    const VERSION_MINOR = 2;
+
+    /**
+     * The constant referencing the major version. Returns 3
+     *
+     * ```php
+     * echo (new Phalcon\Support\Version())
+     *          ->getPart(Phalcon\Support\Version::VERSION_SPECIAL);
+     * ```
+     */
+    const VERSION_SPECIAL = 3;
+
+    /**
+     * The constant referencing the major version. Returns 4
+     *
+     * ```php
+     * echo (new Phalcon\Support\Version())
+     *          ->getPart(Phalcon\Support\Version::VERSION_SPECIAL_NUMBER);
+     * ```
+     */
+    const VERSION_SPECIAL_NUMBER = 4;
+
+    /**
+     * Area where the version number is set. The format is as follows:
+     * ABBCCDE
+     *
+     * A - Major version
+     * B - Med version (two digits)
+     * C - Min version (two digits)
+     * D - Special release: 1 = alpha, 2 = beta, 3 = RC, 4 = stable
+     * E - Special release version i.e. RC1, Beta2 etc.
+     */
+    protected function getVersion(): array
+    {
+        return [5, 0, 0, 1, 4];
+    }
+
+    /**
+     * Translates a number to a special release.
+     */
+    protected final function getSpecial(int $special): string
+    {
+        switch ($special) {
+            case 1:
+                return 'alpha';
+            case 2:
+                return 'beta';
+            case 3:
+                return 'RC';
+        }
+
+        return '';
+    }
+
+    /**
+     * Returns the active version (string)
+     *
+     * ```php
+     * echo (new Phalcon\Version())->get();
+     * ```
+     */
+    public function get(): string
+    {
+        $version = $this->getVersion();
+
+        $major         = $version[self::VERSION_MAJOR];
+        $medium        = $version[self::VERSION_MEDIUM];
+        $minor         = $version[self::VERSION_MINOR];
+        $special       = $version[self::VERSION_SPECIAL];
+        $specialNumber = $version[self::VERSION_SPECIAL_NUMBER];
+
+        $result  = $major . "." . $medium . "." . $minor;
+        $suffix  = $this->getSpecial($special);
+
+        if ('' !== $suffix) {
+            /**
+             * A pre-release version should be denoted by appending alpha/beta or RC and
+             * a patch version.
+             * examples 5.0.0alpha2, 5.0.0beta1, 5.0.0RC3
+             */
+            $result .= $suffix;
+
+            if (0 !== $specialNumber) {
+                $result .= $specialNumber;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns the numeric active version
+     *
+     * ```php
+     * echo (new Phalcon\Version())->getId();
+     * ```
+     */
+    public function getId(): string
+    {
+        $version = $this->getVersion();
+
+        $major         = $version[self::VERSION_MAJOR];
+        $medium        = $version[self::VERSION_MEDIUM];
+        $minor         = $version[self::VERSION_MINOR];
+        $special       = $version[self::VERSION_SPECIAL];
+        $specialNumber = $version[self::VERSION_SPECIAL_NUMBER];
+
+        return $major
+            . sprintf("%02s", $medium)
+            . sprintf("%02s", $minor)
+            . $special
+            . $specialNumber;
+    }
+
+    /**
+     * Returns a specific part of the version. If the wrong parameter is passed
+     * it will return the full version
+     *
+     * ```php
+     * echo (new Phalcon\Version())->getPart(Phalcon\Version::VERSION_MAJOR);
+     * ```
+     */
+    public function getPart(int $part): string
+    {
+        $version = $this->getVersion();
+
+        switch ($part) {
+            case self::VERSION_MAJOR:
+            case self::VERSION_MEDIUM:
+            case self::VERSION_MINOR:
+            case self::VERSION_SPECIAL_NUMBER:
+                return (string) $version[$part];
+
+            case self::VERSION_SPECIAL:
+                return $this->getSpecial($version[self::VERSION_SPECIAL]);
+        }
+
+        return $this->get();
+    }
+}
