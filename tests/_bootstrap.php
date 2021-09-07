e9249f413cd5d5e6e006516ce2c350d28da38f60
Nikolaos Dimopoulos <nikos@niden.net>
2021-09-07 13:20:01 -0400

    build(github actions): refactoring and aligning with cphalcon workflows
    Refs #6.0.x

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $root = dirname(realpath(__DIR__) . DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 //define('PROJECT_PATH', $root);
 
-require_once $root . 'tests/_ci/functions.php';
+require_once $root . 'tests/_config/functions.php';
 
 $folders = [
     cacheDir(),
