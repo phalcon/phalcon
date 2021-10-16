16da52ef296dc9939fd9c400d4c10d92a2acbaf4
Nikolaos Dimopoulos <nikos@niden.net>
2021-10-16 17:10:01 -0400

    refactor(crypt): added crypt fixture for tests; added openssl method helper trait
    Refs #193

---
+++ src/Support/Traits/PhpOpensslTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Support\Traits;
+
+use function openssl_cipher_iv_length;
+use function openssl_random_pseudo_bytes;
+
+/**
+ * Trait PhpOpensslTrait
+ *
+ * @package Phalcon\Support\Traits
+ */
+trait PhpOpensslTrait
+{
+    /**
+     * @param string $cipher
+     *
+     * @return false|int
+     *
+     * @link https://www.php.net/manual/en/function.openssl-cipher-iv-length
+     */
+    public function phpOpensslCipherIvLength(string $cipher)
+    {
+        return openssl_cipher_iv_length($cipher);
+    }
+
+    /**
+     * @param int $length
+     *
+     * @return false|string
+     *
+     * @link https://php.net/manual/en/function.openssl-random-pseudo-bytes
+     */
+    protected function phpOpensslRandomPseudoBytes(int $length)
+    {
+        return openssl_random_pseudo_bytes($length);
+    }
+}
