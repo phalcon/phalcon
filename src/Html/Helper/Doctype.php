2b67fcee9a54cff06fd0da52a4e2c9733c03e9f2
Nikolaos Dimopoulos <nikos@niden.net>
2021-12-03 09:23:50 -0500

    style(helper/doctype): removed conditions and replaced them with array map
    Refs #6.0.x

--- src/Html/Helper/Doctype.php
+++ src/Html/Helper/Doctype.php
@@ -11,8 +11,6 @@ declare(strict_types=1);
 
 namespace Phalcon\Html\Helper;
 
-use Phalcon\Html\Exception;
-
 use const PHP_EOL;
 
 /**
@@ -20,17 +18,17 @@ use const PHP_EOL;
  */
 class Doctype
 {
-    public const HTML32 = 1;
-    public const HTML401_STRICT = 2;
+    public const HTML32               = 1;
+    public const HTML401_STRICT       = 2;
     public const HTML401_TRANSITIONAL = 3;
-    public const HTML401_FRAMESET = 4;
-    public const HTML5 = 5;
-    public const XHTML10_STRICT = 6;
+    public const HTML401_FRAMESET     = 4;
+    public const HTML5                = 5;
+    public const XHTML10_STRICT       = 6;
     public const XHTML10_TRANSITIONAL = 7;
-    public const XHTML10_FRAMESET = 8;
-    public const XHTML11 = 9;
-    public const XHTML20 = 10;
-    public const XHTML5 = 11;
+    public const XHTML10_FRAMESET     = 8;
+    public const XHTML11              = 9;
+    public const XHTML20              = 10;
+    public const XHTML5               = 11;
 
     /**
      * @var string
@@ -52,7 +50,7 @@ class Doctype
         int $flag = self::HTML5,
         string $delimiter = PHP_EOL
     ): void {
-        $this->flag = $flag;
+        $this->flag      = $flag;
         $this->delimiter = $delimiter;
     }
 
@@ -61,54 +59,31 @@ class Doctype
      */
     public function __toString(): string
     {
-        switch ($this->flag) {
-            case self::HTML32:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">"
-                    . $this->delimiter;
-            case self::HTML401_STRICT:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\""
-                    . $this->delimiter
-                    . "\t\"http://www.w3.org/TR/html4/strict.dtd\">"
-                    . $this->delimiter;
-            case self::HTML401_TRANSITIONAL:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\""
-                    . $this->delimiter
-                    . "\t\"http://www.w3.org/TR/html4/loose.dtd\">"
-                    . $this->delimiter;
-            case self::HTML401_FRAMESET:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\""
-                    . $this->delimiter
-                    . "\t\"http://www.w3.org/TR/html4/frameset.dtd\">"
-                    . $this->delimiter;
-            case self::XHTML10_STRICT:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
-                    . $this->delimiter
-                    . "\t\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
-                    . $this->delimiter;
-            case self::XHTML10_TRANSITIONAL:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\""
-                    . $this->delimiter
-                    . "\t\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
-                    . $this->delimiter;
-            case self::XHTML10_FRAMESET:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset//EN\""
-                    . $this->delimiter
-                    . "\t\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">"
-                    . $this->delimiter;
-            case self::XHTML11:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\""
-                    . $this->delimiter
-                    . "\t\"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">"
-                    . $this->delimiter;
-            case self::XHTML20:
-                return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 2.0//EN\""
-                    . $this->delimiter
-                    . "\t\"http://www.w3.org/MarkUp/DTD/xhtml2.dtd\">"
-                    . $this->delimiter;
-            case self::HTML5:
-            case self::XHTML5:
-            default:
-                return "<!DOCTYPE html>" . $this->delimiter;
-        }
+        $prefix = "<!DOCTYPE html PUBLIC \"-//W3C//DTD ";
+        $dlm    = $this->delimiter;
+        $map    = [
+            self::HTML32               => $prefix . "HTML 3.2 Final//EN\">" . $dlm,
+            self::HTML401_STRICT       => $prefix . "HTML 4.01//EN\"" . $dlm
+                . "\t\"http://www.w3.org/TR/html4/strict.dtd\">" . $dlm,
+            self::HTML401_TRANSITIONAL => $prefix . "HTML 4.01 Transitional//EN\"" . $dlm
+                . "\t\"http://www.w3.org/TR/html4/loose.dtd\">" . $dlm,
+            self::HTML401_FRAMESET     => $prefix . "HTML 4.01 Frameset//EN\"" . $dlm
+                . "\t\"http://www.w3.org/TR/html4/frameset.dtd\">" . $dlm,
+            self::XHTML10_STRICT       => $prefix . "XHTML 1.0 Strict//EN\"" . $dlm
+                . "\t\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" . $dlm,
+            self::XHTML10_TRANSITIONAL => $prefix . "XHTML 1.0 Transitional//EN\"" . $dlm
+                . "\t\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" . $dlm,
+            self::XHTML10_FRAMESET     => $prefix . "XHTML 1.0 Frameset//EN\"" . $dlm
+                . "\t\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">" . $dlm,
+            self::XHTML11              => $prefix . "XHTML 1.1//EN\"" . $dlm
+                . "\t\"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">" . $dlm,
+            self::XHTML20              => $prefix . "XHTML 2.0//EN\"" . $dlm
+                . "\t\"http://www.w3.org/MarkUp/DTD/xhtml2.dtd\">" . $dlm,
+        ];
+
+        /**
+         * Default is HTML5
+         */
+        return $map[$this->flag] ?? "<!DOCTYPE html>" . $dlm;
     }
 }
