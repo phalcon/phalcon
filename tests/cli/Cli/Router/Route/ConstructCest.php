a2d32d8c3c62cc1c0a714fb142eb78fa6f57ad75
Nikolaos Dimopoulos <nikos@niden.net>
2022-10-16 11:38:03 -0400

    [#6] - added tests for Cli/Router/Route

---
+++ tests/cli/Cli/Router/Route/ConstructCest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Cli\Cli\Router\Route;
+
+use CliTester;
+use Phalcon\Cli\Router\Route;
+use Phalcon\Cli\Router\RouteInterface;
+
+class ConstructCest
+{
+    /**
+     * Tests Phalcon\Cli\Router\Route :: __construct()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-01-05
+     */
+    public function cliRouterRouteConstruct(CliTester $I)
+    {
+        $I->wantToTest('Cli\Router\Route - __construct()');
+
+        $route = new Route('test');
+
+        $class = RouteInterface::class;
+        $I->assertInstanceOf($class, $route);
+
+        $class = Route::class;
+        $I->assertInstanceOf($class, $route);
+    }
+}
