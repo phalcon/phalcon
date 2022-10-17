7e7cc636d4d9a30e030fa9afd1119c36d3e1a958
Nikolaos Dimopoulos <nikos@niden.net>
2022-10-17 14:55:18 -0400

    [#6] - adding binder interfaces needed for cli

---
+++ src/Mvc/Model/BinderInterface.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Mvc\Model;
+
+use Phalcon\Cache\Adapter\AdapterInterface;
+
+/**
+ * Phalcon\Mvc\Model\BinderInterface
+ *
+ * Interface for Phalcon\Mvc\Model\Binder
+ */
+interface BinderInterface
+{
+    /**
+     * Bind models into params in proper handler
+     *
+     * @param object      $handler
+     * @param array       $params
+     * @param string      $cacheKey
+     * @param string|null $methodName
+     *
+     * @return array
+     */
+    public function bindToHandler(
+        object $handler,
+        array $params,
+        string $cacheKey,
+        ?string $methodName = null
+    ): array;
+
+    /**
+     * Gets active bound models
+     *
+     * @return array
+     */
+    public function getBoundModels(): array;
+
+    /**
+     * Gets cache instance
+     *
+     * @return AdapterInterface
+     */
+    public function getCache(): AdapterInterface;
+
+    /**
+     * Sets cache instance
+     *
+     * @param AdapterInterface $cache
+     *
+     * @return BinderInterface
+     */
+    public function setCache(AdapterInterface $cache): BinderInterface;
+}
---
+++ src/Mvc/ModuleDefinitionInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Mvc;
+
+use Phalcon\Di\DiInterface;
+
+/**
+ * This interface must be implemented by class module definitions
+ */
+interface ModuleDefinitionInterface
+{
+    /**
+     * Registers an autoloader related to the module
+     *
+     * @param DiInterface|null $container
+     *
+     * @return void
+     */
+    public function registerAutoloaders(DiInterface $container = null);
+
+    /**
+     * Registers services related to the module
+     *
+     * @param DiInterface $container
+     *
+     * @return void
+     */
+    public function registerServices(DiInterface $container);
+}
