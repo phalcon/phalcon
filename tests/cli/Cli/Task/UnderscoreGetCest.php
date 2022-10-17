02256ea2e84ec6c6eeb368452b38d6d3a06e95d4
Nikolaos Dimopoulos <nikos@niden.net>
2022-10-17 11:16:08 -0400

    [#6] - added Task and related tests

---
+++ tests/cli/Cli/Task/UnderscoreGetCest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Cli\Cli\Task;
+
+use CliTester;
+use Phalcon\Cli\Task;
+use Phalcon\Di\FactoryDefault\Cli as CliDi;
+use Phalcon\Events\Manager;
+
+class UnderscoreGetCest
+{
+    /**
+     * Tests Phalcon\Cli\Task :: __get()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function cliTaskUnderscoreGet(CliTester $I)
+    {
+        $I->wantToTest('Cli\Task - __get()');
+
+        $task      = new Task();
+        $container = new CliDi();
+        $task->setDi($container);
+
+        $eventsManager = new Manager();
+        $task->setEventsManager($eventsManager);
+
+        $expected = $container;
+        $actual   = $task->di;
+        $I->assertSame($expected, $actual);
+
+        $expected = $eventsManager;
+        $actual   = $task->eventsManager;
+        $I->assertSame($expected, $actual);
+    }
+}
