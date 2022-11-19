92d5b44b48d1224cdc1c0b25f0e032c505895441
Nikolaos Dimopoulos <nikos@niden.net>
2022-11-19 15:12:16 -0500

    [#6.0.x] - adding benchmarks

---
+++ tests/benchmarks/Support/CollectionBench.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Benchmarks;
+
+use Phalcon\Support\Collection;
+
+use function rand;
+use function range;
+use function uniqid;
+
+/**
+ * @BeforeMethods({"setUp"})
+ */
+class CollectionBench
+{
+    private array $data = [];
+
+    public function setUp(): void
+    {
+        $letters = 'abcdefghijklmnopqrstuvwxyz';
+        for ($step = 1; $step <= 100; $step++) {
+            $prefix = substr($letters, rand(0, 23), 1) . $step;
+            $this->data[$prefix] = uniqid('col-');
+        }
+    }
+
+    /**
+     * @Revs(1000)
+     * @Iterations(5)
+     *
+     * @return void
+     */
+    public function benchConstruct(): void
+    {
+        $collection = new Collection();
+    }
+    /**
+     * @Revs(1000)
+     * @Iterations(5)
+     *
+     * @return void
+     */
+    public function benchConstructWithData(): void
+    {
+        $collection = new Collection($this->data);
+    }
+}
