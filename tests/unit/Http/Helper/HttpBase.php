39c338a481f40ecabcad5abb6ccb650328725193
Nikolaos Dimopoulos <nikos@niden.net>
2023-01-22 18:42:08 -0500

    [#24] - removed unused methods

--- tests/unit/Http/Helper/HttpBase.php
+++ tests/unit/Http/Helper/HttpBase.php
@@ -13,11 +13,17 @@ declare(strict_types=1);
 
 namespace Phalcon\Tests\Unit\Http\Helper;
 
-use Phalcon\Http\Response;
+use Page\Http;
+use Phalcon\Http\Request;
+use Phalcon\Tests\Fixtures\Http\PhpStream;
 use Phalcon\Tests\Fixtures\Traits\DiTrait;
 use UnitTester;
 
 use function header_remove;
+use function stream_wrapper_register;
+use function stream_wrapper_restore;
+use function stream_wrapper_unregister;
+use function time;
 
 class HttpBase
 {
@@ -30,17 +36,22 @@ class HttpBase
      */
     public function _before(UnitTester $I)
     {
-        $this->store["SERVER"]  = $_SERVER ?? [];
-        $this->store["REQUEST"] = $_REQUEST ?? [];
-        $this->store["GET"]     = $_GET ?? [];
-        $this->store["POST"]    = $_POST ?? [];
-        $this->store["COOKIE"]  = $_COOKIE ?? [];
+        $this->store['SERVER']  = $_SERVER ?? [];
+        $this->store['REQUEST'] = $_REQUEST ?? [];
+        $this->store['GET']     = $_GET ?? [];
+        $this->store['POST']    = $_POST ?? [];
+        $this->store['COOKIE']  = $_COOKIE ?? [];
+        $this->store['FILES']   = $_FILES ?? [];
 
-        $_SERVER  = [];
+        $time    = $_SERVER['REQUEST_TIME_FLOAT'] ?? time();
+        $_SERVER = [
+            'REQUEST_TIME_FLOAT' => $time,
+        ];
         $_REQUEST = [];
         $_GET     = [];
         $_POST    = [];
         $_COOKIE  = [];
+        $_FILES   = [];
 
         header_remove();
 
@@ -52,66 +63,12 @@ class HttpBase
      */
     public function _after(UnitTester $I)
     {
-        $_SERVER  = $this->store["SERVER"];
-        $_REQUEST = $this->store["REQUEST"];
-        $_GET     = $this->store["GET"];
-        $_POST    = $this->store["POST"];
-        $_COOKIE  = $this->store["COOKIE"];
-    }
-
-    /**
-     * Initializes the response object and returns it
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function getResponseObject(): Response
-    {
-        return $this->container->get('response');
-    }
-
-    /**
-     * Checks the has functions on non defined variables
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function hasEmpty(UnitTester $I, string $function)
-    {
-        $request = $this->getRequestObject();
-
-        $I->assertFalse(
-            $request->$function('test')
-        );
-    }
-
-    /**
-     * Initializes the request object and returns it
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function getRequestObject(): Request
-    {
-        return $this->container->get('request');
-    }
-
-    /**
-     * Checks the has functions on defined variables
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function hasNotEmpty(UnitTester $I, string $function, string $method)
-    {
-        $request  = $this->getRequestObject();
-        $unMethod = "un{$method}";
-
-        $this->$method('test', 1);
-        $actual = $request->$function('test');
-        $this->$unMethod('test');
-
-        $I->assertTrue($actual);
+        $_SERVER  = $this->store['SERVER'];
+        $_REQUEST = $this->store['REQUEST'];
+        $_GET     = $this->store['GET'];
+        $_POST    = $this->store['POST'];
+        $_COOKIE  = $this->store['COOKIE'];
+        $_FILES   = $this->store['FILES'];
     }
 
     /**
@@ -144,7 +101,62 @@ class HttpBase
         $actual = $request->$function('test');
         $this->$unMethod('test');
 
-        $I->assertEquals(1, $actual);
+        $I->assertSame(1, $actual);
+    }
+
+    /**
+     * Initializes the request object and returns it
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2014-10-05
+     */
+    protected function getRequestObject(): Request
+    {
+        return $this->container->get('request');
+    }
+
+    /**
+     * Initializes the response object and returns it
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2014-10-05
+     */
+    protected function getResponseObject(): Response
+    {
+        return $this->container->get('response');
+    }
+
+    /**
+     * Checks the has functions on non defined variables
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2014-10-05
+     */
+    protected function hasEmpty(UnitTester $I, string $function)
+    {
+        $request = $this->getRequestObject();
+
+        $I->assertFalse(
+            $request->$function('test')
+        );
+    }
+
+    /**
+     * Checks the has functions on defined variables
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2014-10-05
+     */
+    protected function hasNotEmpty(UnitTester $I, string $function, string $method)
+    {
+        $request  = $this->getRequestObject();
+        $unMethod = "un{$method}";
+
+        $this->$method('test', 1);
+        $actual = $request->$function('test');
+        $this->$unMethod('test');
+
+        $I->assertTrue($actual);
     }
 
     /**
@@ -159,11 +171,11 @@ class HttpBase
         $unMethod = "un{$method}";
 
         $this->$method('test', 'lol<');
-        $expected = 'lol';
+        $expected = 'lol&lt;';
         $actual   = $request->$function('test', 'string');
         $this->$unMethod('test');
 
-        $I->assertEquals($expected, $actual);
+        $I->assertSame($expected, $actual);
     }
 
     /**
@@ -180,114 +192,27 @@ class HttpBase
         $unMethod = "un{$method}";
 
         $this->$method('test', 'lol<');
-        $expected = 'lol';
+        $expected = 'lol&lt;';
         $actual   = $request->$function('test', $filter);
         $this->$unMethod('test');
 
-        $I->assertEquals($expected, $actual);
+        $I->assertSame($expected, $actual);
     }
 
     /**
-     * Sets a server variable ($_SERVER)
-     *
-     * @param string $var
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
+     * @return void
      */
-    protected function setServerVar($var, $value)
+    protected function registerStream(): void
     {
-        $_SERVER[$var] = $value;
+        stream_wrapper_unregister(Http::STREAM_NAME);
+        stream_wrapper_register(Http::STREAM_NAME, PhpStream::class);
     }
 
     /**
-     * Unsets a server variable ($_SERVER)
-     *
-     * @param string $var
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
+     * @return void
      */
-    protected function unsetServerVar($var)
+    protected function unregisterStream(): void
     {
-        unset($_SERVER[$var]);
-    }
-
-    /**
-     * Sets a get variable ($_GET)
-     *
-     * @param string $var
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function setGetVar($var, $value)
-    {
-        $_GET[$var] = $value;
-    }
-
-    /**
-     * Unsets a get variable ($_GET)
-     *
-     * @param string $var
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function unsetGetVar($var)
-    {
-        unset($_GET[$var]);
-    }
-
-    /**
-     * Sets a post variable ($_POST)
-     *
-     * @param string $var
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function setPostVar($var, $value)
-    {
-        $_POST[$var] = $value;
-    }
-
-    /**
-     * Unsets a post variable ($_POST)
-     *
-     * @param string $var
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function unsetPostVar($var)
-    {
-        unset($_POST[$var]);
-    }
-
-    /**
-     * Sets a request variable ($_REQUEST)
-     *
-     * @param string $var
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function setRequestVar($var, $value)
-    {
-        $_REQUEST[$var] = $value;
-    }
-
-    /**
-     * Unsets a request variable ($_REQUEST)
-     *
-     * @param string $var
-     *
-     * @author Phalcon Team <team@phalcon.io>
-     * @since  2014-10-05
-     */
-    protected function unsetRequestVar($var)
-    {
-        unset($_REQUEST[$var]);
+        stream_wrapper_restore(Http::STREAM_NAME);
     }
 }
