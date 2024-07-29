89b81ee97e103ac6d4ff915ab38c661b6fb21d55
Nikolaos Dimopoulos <nikos@niden.net>
2024-07-29 18:07:38 -0500

    [#49] - adding exceptions

---
+++ src/Container/Exception/Exception.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Implementation of this file has been influenced by CapsulePHP
+ *
+ * @link    https://github.com/capsulephp/di
+ * @license https://github.com/capsulephp/di/blob/3.x/LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Container\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+
+abstract class Exception extends \Exception implements ContainerExceptionInterface
+{
+}
