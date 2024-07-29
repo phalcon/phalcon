409f92a477f5e9f49fb40d971cde92abdb8a6b1f
Nikolaos Dimopoulos <nikos@niden.net>
2024-07-29 17:27:23 -0500

    [#49] - fixtures for container

---
+++ tests/_data/fixtures/Container/TestProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Tests\Fixtures\Container;
+
+use Phalcon\Container\Container;
+use Phalcon\Container\Definitions\Definitions;
+use Phalcon\Container\Interfaces\ProviderInterface;
+
+class TestProvider implements ProviderInterface
+{
+    public function provide(Definitions $definitions) : void
+    {
+        $definitions->{TestWithInterface::class}->argument(0, 'ten');
+        $definitions->oneval = 'oneval';
+        $definitions->lazyval = $definitions->call(
+            function (Container $container) {
+                return 'lazyval';
+            }
+        );
+    }
+}
---
+++ tests/_data/fixtures/Container/TestWithInterface.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Phalcon\Tests\Fixtures\Container;
+
+class TestWithInterface implements TestInterface
+{
+    /**
+     * @param string $one
+     * @param string $two
+     */
+    public function __construct(
+        public string $one,
+        public string $two = 'two'
+    ) {
+    }
+
+    /**
+     * @param string $suffix
+     *
+     * @return void
+     */
+    public function append(string $suffix) : void
+    {
+        $this->one .= $suffix;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue() : string
+    {
+        return $this->two;
+    }
+
+    public static function staticMethod(string $word) : string
+    {
+        return $word;
+    }
+}
