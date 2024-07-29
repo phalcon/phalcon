3dc492f63e215a65cebc2cdace91cca3d8e00bf9
Nikolaos Dimopoulos <nikos@niden.net>
2024-07-29 18:07:52 -0500

    [#49] - interfaces

---
+++ src/Container/Interfaces/ProviderInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Implementation of this file has been influenced by CapsulePHP
+ *
+ * @link    https://github.com/capsulephp/di
+ * @license https://github.com/capsulephp/di/blob/3.x/LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Container\Interfaces;
+
+use Phalcon\Container\Definitions\Definitions;
+
+interface ProviderInterface
+{
+    /**
+     * @param Definitions $definitions
+     *
+     * @return void
+     */
+    public function provide(Definitions $definitions): void;
+}
