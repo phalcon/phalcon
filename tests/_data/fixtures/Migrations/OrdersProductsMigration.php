916697a342ec0bf0779b06f2a6ca05b0ee944940
noone-silent <lominum@protonmail.com>
2024-07-30 09:33:07 +0700

    refactor(builder): refactor from zephir.

--- tests/_data/fixtures/Migrations/OrdersProductsMigration.php
+++ tests/_data/fixtures/Migrations/OrdersProductsMigration.php
@@ -49,10 +49,10 @@ SQL;
     {
         return [
             "
-drop table if exists private.`co_orders_x_products`;
+drop table if exists `co_orders_x_products`;
             ",
             "
-CREATE TABLE private.`co_orders_x_products` (
+CREATE TABLE `co_orders_x_products` (
   `oxp_ord_id` int(10) unsigned NOT NULL,
   `oxp_prd_id` int(10) unsigned NOT NULL,
   `oxp_quantity` int(10) unsigned NOT NULL,
