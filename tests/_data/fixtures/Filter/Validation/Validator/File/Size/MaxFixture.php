015a732ae7ade0d3956b253eb8e7ab47551e711a
Nikolaos Dimopoulos <nikos@niden.net>
2024-08-04 18:47:09 -0500

    [#ntegration] - validation fixtures

---
+++ tests/_data/fixtures/Filter/Validation/Validator/File/Size/MaxFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Fixtures\Filter\Validation\Validator\File\Size;
+
+use Phalcon\Filter\Validation\Validator\File\Size\Max;
+
+class MaxFixture extends Max
+{
+    /**
+     * Checks if a file has been uploaded; Internal check that can be
+     * overridden in a subclass if you do not want to check uploaded files
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    protected function checkIsUploadedFile(string $name): bool
+    {
+        return false;
+    }
+}
