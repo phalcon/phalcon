cdda1cb2a6aca40a06495e5709d04ea2f464221d
Nikolaos Dimopoulos <nikos@niden.net>
2024-08-05 18:07:54 -0500

    [#ntegration] - mvc application tests

---
+++ tests/unit/Mvc/Application/GetModulesTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Mvc\Application;
+
+use Phalcon\Tests\UnitTestCase;
+use Phalcon\Mvc\Application;
+use Phalcon\Tests\Modules\Frontend\Module;
+
+class GetModulesTest extends UnitTestCase
+{
+    /**
+     * Tests Phalcon\Mvc\Application :: getModules() - empty
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     *
+     * @author Nathan Edwards <https://github.com/npfedwards>
+     * @since  2018-12-26
+     */
+    public function testMvcApplicationGetModulesEmpty(): void
+    {
+        $application = new Application();
+
+        $this->assertEquals(
+            [],
+            $application->getModules()
+        );
+    }
+
+    /**
+     * Tests Phalcon\Mvc\Application :: getModules()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     *
+     * @author Nathan Edwards <https://github.com/npfedwards>
+     * @since  2018-12-26
+     */
+    public function testMvcApplicationGetModules(): void
+    {
+        $application = new Application();
+
+        $definition = [
+            'frontend' => [
+                'className' => Module::class,
+                'path'      => dataDir('fixtures/modules/frontend/Module.php'),
+            ],
+            'backend'  => [
+                'className' => \Phalcon\Tests\Modules\Backend\Module::class,
+                'path'      => dataDir('fixtures/modules/backend/Module.php'),
+            ],
+        ];
+
+        $application->registerModules($definition);
+
+        $this->assertEquals(
+            $definition,
+            $application->getModules()
+        );
+    }
+}
---
+++ tests/unit/Mvc/Application/SetDefaultModuleTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Mvc\Application;
+
+use Phalcon\Tests\UnitTestCase;
+
+class SetDefaultModuleTest extends UnitTestCase
+{
+    /**
+     * Tests Phalcon\Mvc\Application :: setDefaultModule()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function testMvcApplicationSetDefaultModule(): void
+    {
+        $this->markTestSkipped('Need implementation');
+    }
+}
