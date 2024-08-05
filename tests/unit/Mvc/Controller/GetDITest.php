c4456c645d55af20beb5abb9207497f8fde5929d
Nikolaos Dimopoulos <nikos@niden.net>
2024-08-05 18:08:23 -0500

    [#ntegration] -  added mvc controller tests

---
+++ tests/unit/Mvc/Controller/GetDITest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Mvc\Controller;
+
+use Phalcon\Tests\UnitTestCase;
+
+class GetDITest extends UnitTestCase
+{
+    /**
+     * Tests Phalcon\Mvc\Controller :: getDI()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function testMvcControllerGetDI(): void
+    {
+        $this->markTestSkipped('Need implementation');
+    }
+}
---
+++ tests/unit/Mvc/Controller/UnderscoreGetTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Mvc\Controller;
+
+use Phalcon\Tests\UnitTestCase;
+
+class UnderscoreGetTest extends UnitTestCase
+{
+    /**
+     * Tests Phalcon\Mvc\Controller :: __get()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2018-11-13
+     */
+    public function testMvcControllerUnderscoreGet(): void
+    {
+        $this->markTestSkipped('Need implementation');
+    }
+}
