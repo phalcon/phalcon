907faa4d91638aa88c20ba191a769826e85b0f41
Nikolaos Dimopoulos <nikos@niden.net>
2024-08-06 11:29:51 -0500

    [#ntegration] - reworked fixtures

--- tests/_data/assets/schemas/mysql.sql
+++ tests/_data/assets/schemas/mysql.sql
@@ -1,8 +1,25 @@
+set foreign_key_checks = 0;
 
-
+drop table if exists `album_photo`;
 drop table if exists `album`;
-            
-CREATE TABLE `album` (
+drop table if exists `photo`;
+drop table if exists `complex_default`;
+drop table if exists `co_customers_defaults`;
+drop table if exists `co_customers`;
+drop table if exists `co_dialect`;
+drop table if exists `fractal_dates`;
+drop table if exists `co_invoices`;
+drop table if exists `objects`;
+drop table if exists `co_orders`;
+drop table if exists `co_orders_x_products`;
+drop table if exists `co_products`;
+drop table if exists `co_rb_test_model`;
+drop table if exists `co_setters`;
+drop table if exists `co_sources`;
+drop table if exists `table_with_uuid_primary`;
+drop table if exists `stuff`;
+
+create table `album` (
 	`id`       int(11) UNSIGNED not null AUTO_INCREMENT,
 	`name`     varchar(100)     not null collate 'utf8mb4_unicode_520_ci',
 	`album_id` int(11) unsigned null default null,
@@ -12,13 +29,9 @@ CREATE TABLE `album` (
 	index `album_ibfk_2` (`photo_id`) using BTREE,
 	constraint `album_ibfk_1` foreign key (`album_id`) references `album` (`id`) on update cascade on delete cascade,
 	constraint `album_ibfk_2` foreign key (`photo_id`) references `photo` (`id`) on update cascade on delete set null
-) collate='utf8mb4_unicode_520_ci';
-            
+);
 
-
-drop table if exists `album_photo`;
-            
-CREATE TABLE `album_photo` (
+create table `album_photo` (
 	`id`       int(11) unsigned not null AUTO_INCREMENT,
 	`photo_id` int(11) unsigned null default null,
 	`album_id` int(11) unsigned null default null,
@@ -29,12 +42,8 @@ CREATE TABLE `album_photo` (
 	index `index_foreignkey_album_photo_album` (`album_id`) using BTREE,
 	constraint `c_fk_album_photo_album_id` foreign key (`album_id`) references `album` (`id`) on update cascade on delete cascade,
 	constraint `c_fk_album_photo_photo_id` foreign key (`photo_id`) references `photo` (`id`) on update cascade on delete cascade
-) collate='utf8mb4_unicode_520_ci';
-            
+);
 
-
-drop table if exists `complex_default`;
-            
 create table complex_default
 (
     `id`           int(10) auto_increment primary key,
@@ -42,11 +51,7 @@ create table complex_default
     `updated`      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `updated_null` TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 );
-            
 
-
-drop table if exists `co_customers_defaults`;
-            
 create table co_customers_defaults
 (
     `cst_id`          int(10) auto_increment primary key,
@@ -54,20 +59,16 @@ create table co_customers_defaults
     `cst_name_last`   varchar(100) not null DEFAULT 'cst_default_lastName',
     `cst_name_first`  varchar(50)  not null DEFAULT 'cst_default_firstName'
 );
-            
+
 create index co_customers_defaults_cst_status_flag_index
     on `co_customers_defaults` (`cst_status_flag`);
-            
+
 create index co_customers_defaults_cst_name_last_index
     on `co_customers_defaults` (`cst_name_last`);
-            
+
 create index co_customers_defaults_cst_name_first_index
     on `co_customers_defaults` (`cst_name_first`);
-            
 
-
-drop table if exists `co_customers`;
-            
 create table co_customers
 (
     `cst_id`          int(10) auto_increment primary key,
@@ -75,20 +76,16 @@ create table co_customers
     `cst_name_last`   varchar(100) null,
     `cst_name_first`  varchar(50)  null
 );
-            
+
 create index co_customers_cst_status_flag_index
     on `co_customers` (`cst_status_flag`);
-            
+
 create index co_customers_cst_name_last_index
     on `co_customers` (`cst_name_last`);
-            
+
 create index co_customers_cst_name_first_index
     on `co_customers` (`cst_name_first`);
-            
 
-
-drop table if exists `co_dialect`;
-            
 create table co_dialect
 (
   field_primary            int auto_increment primary key,
@@ -134,12 +131,8 @@ create table co_dialect
   unique key dialect_table_unique (field_integer),
   key dialect_table_index (field_bigint),
   key dialect_table_two_fields (field_char, field_char_default)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            
+);
 
-
-drop table if exists `fractal_dates`;
-            
 create table fractal_dates
 (
     `id`           int(10)      auto_increment primary key,
@@ -147,11 +140,7 @@ create table fractal_dates
     `fdatetime`    datetime(2)  null,
     `ftimestamp`   timestamp(2) null
 );
-            
 
-
-drop table if exists `co_invoices`;
-            
 create table co_invoices
 (
     `inv_id`          int(10) auto_increment primary key,
@@ -161,53 +150,38 @@ create table co_invoices
     `inv_total`       float(10, 2) null,
     `inv_created_at`  datetime     null
 );
-            
+
 create index co_invoices_inv_cst_id_index
     on `co_invoices` (`inv_cst_id`);
-            
+
 create index co_invoices_inv_status_flag_index
     on `co_invoices` (`inv_status_flag`);
-            
+
 create index co_invoices_inv_created_at_index
     on `co_invoices` (`inv_created_at`);
-            
 
-
-drop table if exists objects;
-            
-create table objects
+create table `objects`
 (
     `obj_id`   int(10) auto_increment primary key,
     `obj_name` varchar(100) not null,
     `obj_type` tinyint(3) unsigned not null
 );
-            
 
-
-drop table if exists `co_orders`;
-            
-CREATE TABLE `co_orders` (
+create table `co_orders` (
     `ord_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `ord_name` VARCHAR(70) NULL,
     PRIMARY KEY (`ord_id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            
+);
 
-
-drop table if exists private.`co_orders_x_products`;
-            
-CREATE TABLE private.`co_orders_x_products` (
+create table `co_orders_x_products` (
   `oxp_ord_id` int(10) unsigned NOT NULL,
   `oxp_prd_id` int(10) unsigned NOT NULL,
   `oxp_quantity` int(10) unsigned NOT NULL,
   PRIMARY KEY (`oxp_ord_id`, `oxp_prd_id` )
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            
+);
 
 
-drop table if exists `photo`;
-            
-CREATE TABLE `photo` (
+create table `photo` (
 	`id`                int(10) unsigned not null auto_increment,
 	`date_uploaded`     datetime not null default current_timestamp(),
 	`original_filename` text not null collate 'utf8mb4_unicode_520_ci',
@@ -226,24 +200,19 @@ CREATE TABLE `photo` (
 	`battles`           int(10) unsigned not null DEFAULT '0',
 	`wins`              int(10) unsigned not null DEFAULT '0',
 	primary key (`id`) using BTREE
-) collate='utf8mb4_unicode_520_ci';
-            
+);
 
-
-drop table if exists `co_products`;
-            
-CREATE TABLE `co_products` (
+create table `co_products` (
     `prd_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `prd_name` VARCHAR(70) NULL,
     PRIMARY KEY (`prd_id`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            
+);
 
-DROP TABLE IF EXISTS co_rb_test_model;CREATE TABLE co_rb_test_model (id SMALLINT, name VARCHAR(10) NOT NULL);
+create table co_rb_test_model (
+    `id`   smallint,
+    `name` varchar(10) not null
+);
 
-
-drop table if exists `co_setters`;
-            
 create table co_setters
 (
     `id`       int(10) auto_increment primary key,
@@ -251,39 +220,26 @@ create table co_setters
     `column2`  varchar(100) null,
     `column3`  varchar(100) null
 );
-            
 
-
-drop table if exists `co_sources`;
-            
 create table co_sources
 (
     `id`       int(10) auto_increment primary key,
     `username` varchar(100) null,
     `source`   varchar(100) null
 );
-            
+
 create index co_sources_username_index
     on co_sources (username);
-            
 
-
-drop table if exists `table_with_uuid_primary`;
-            
 create table table_with_uuid_primary
 (
     `uuid`          char(36) not null primary key,
     `int_field`     int null
 );
-            
 
-
-drop table if exists `stuff`;
-            
 create table stuff
 (
     `stf_id`   int(10) auto_increment primary key,
     `stf_name` varchar(100) not null,
     `stf_type` tinyint(3) unsigned not null
 );
-            
