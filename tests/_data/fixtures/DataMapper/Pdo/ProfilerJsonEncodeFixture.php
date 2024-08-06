fc638a0afa066cce297d0ffcbc4d65c0838b061b
Nikolaos Dimopoulos <nikos@niden.net>
2024-08-06 11:29:51 -0500

    [#ntegration] - reworked fixtures

---
+++ tests/_data/fixtures/DataMapper/Pdo/ProfilerJsonEncodeFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+* This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Fixtures\DataMapper\Pdo;
+
+use Phalcon\DataMapper\Pdo\Profiler\Profiler;
+
+class ProfilerJsonEncodeFixture extends Profiler
+{
+    /**
+     * @param mixed $value
+     * @param int   $flags
+     * @param int   $depth
+     *
+     * @return false|string
+     *
+     * @link https://php.net/manual/en/function.json-encode.php
+     */
+    protected function phpJsonEncode($value, int $flags = 0, int $depth = 512)
+    {
+        return false;
+    }
+}
