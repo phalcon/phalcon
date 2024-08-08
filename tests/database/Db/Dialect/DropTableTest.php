8001b7834637875d420b93d2942b6d55172e3c1a
Nikolaos Dimopoulos <nikos@niden.net>
2024-08-08 09:59:34 -0500

    [#uality] - dialect dropPK and dropTable tests

---
+++ tests/database/Db/Dialect/DropTableTest.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Database\Db\Dialect;
+
+use Phalcon\Db\Dialect\Mysql;
+use Phalcon\Tests\DatabaseTestCase;
+
+final class DropTableTest extends DatabaseTestCase
+{
+    /**
+     * Tests Phalcon\Db\Dialect :: dropTable
+     *
+     * @dataProvider getDialects
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-01-20
+     *
+     * @group  common
+     */
+    public function testDbDialectDropTable(
+        string $dialectClass,
+        string $expected
+    ): void {
+        /** @var Mysql $dialect */
+        $dialect = new $dialectClass();
+
+        $actual  = $dialect->dropTable('table', 'schema');
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Tests Phalcon\Db\Dialect :: dropTable
+     *
+     * @dataProvider getDialectsNotExists
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-01-20
+     *
+     * @group  common
+     */
+    public function testDbDialectDropTableNotExists(
+        string $dialectClass,
+        string $expected
+    ): void {
+        /** @var Mysql $dialect */
+        $dialect = new $dialectClass();
+
+        $actual  = $dialect->dropTable('table', 'schema', false);
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @return array[]
+     */
+    public static function getDialects(): array
+    {
+        return [
+            [
+                Mysql::class,
+                'DROP TABLE IF EXISTS `schema`.`table`'
+
+            ],
+            //            [Postgresql::class],
+            //            [Sqlite::class],
+        ];
+    }
+
+    /**
+     * @return array[]
+     */
+    public static function getDialectsNotExists(): array
+    {
+        return [
+            [
+                Mysql::class,
+                'DROP TABLE `schema`.`table`'
+
+            ],
+            //            [Postgresql::class],
+            //            [Sqlite::class],
+        ];
+    }
+}
