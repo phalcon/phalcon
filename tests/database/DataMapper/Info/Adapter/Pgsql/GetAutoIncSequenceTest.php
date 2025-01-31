efa23498317d301c7bed1d469aca52e9af72c483
Nikolaos Dimopoulos <nikos@niden.net>
2025-01-31 15:43:39 -0600

    adding postgres tests

---
+++ tests/database/DataMapper/Info/Adapter/Pgsql/GetAutoIncSequenceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Database\DataMapper\Info\Adapter\Pgsql;
+
+use Phalcon\DataMapper\Info\Adapter\Pgsql;
+use Phalcon\DataMapper\Pdo\Exception\Exception;
+use Phalcon\Tests\AbstractDatabaseTestCase;
+
+final class GetAutoIncSequenceTest extends AbstractDatabaseTestCase
+{
+    /**
+     * @since  2025-01-14
+     *
+     * @group  pgsql
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function testDmInfoAdapterPgsqlGetAutoIncSequence(): void
+    {
+        $connection = self::getDataMapperConnection();
+
+        $pgsql  = new Pgsql($connection);
+        $schema = $pgsql->getCurrentSchema();
+
+        $expected = 'field_primary';
+        $actual = $pgsql->getAutoincSequence($schema, 'co_dialect');
+        $this->assertSame($expected, $actual);
+
+        $actual = $pgsql->getAutoincSequence($schema, 'co_rb_test_model');
+        $this->assertNull($actual);
+    }
+}
