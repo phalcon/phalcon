ef1a70caee99f3da32908c5c44d106cedf66207c
Nikolaos Dimopoulos <nikos@niden.net>
2026-04-04 21:42:58 -0500

    [#6.0.x] - support files from cphalcon

---
+++ tests/support/Migrations/ArtistsMigration.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Support\Migrations;
+
+use PHPUnit\Framework\Assert;
+
+/**
+ * Class ArtistsMigration
+ */
+class ArtistsMigration extends AbstractMigration
+{
+    protected $table = 'artists';
+
+    /**
+     * @param int    $id
+     * @param string $name
+     *
+     * @return int
+     */
+    public function insert(int $id, string $name): int
+    {
+        $sql = <<<SQL
+insert into artists (id, name) values ({$id}, '{$name}')
+SQL;
+
+        if (!$result = $this->connection->exec($sql)) {
+            $table  = $this->getTable();
+            $driver = $this->getDriverName();
+            Assert::fail(
+                sprintf(
+                    "Failed to insert row #%d into table '%s' using '%s' driver",
+                    $id,
+                    $table,
+                    $driver
+                )
+            );
+        }
+
+        return $result;
+    }
+
+    protected function getSqlMysql(): array
+    {
+        return [
+            "
+drop table if exists `artists`;
+            ",
+            "
+create table `artists`
+(
+    `id`   int(10) auto_increment primary key,
+    `name` varchar(100) not null
+);
+            ",
+        ];
+    }
+
+    protected function getSqlSqlite(): array
+    {
+        return [
+            "
+drop table if exists artists;
+            ",
+            "
+create table artists
+(
+    id   integer constraint artists_pk primary key autoincrement not null,
+    name text not null
+);
+            ",
+        ];
+    }
+
+    protected function getSqlPgsql(): array
+    {
+        return [
+            "
+drop table if exists artists;
+            ",
+            "
+create table artists
+(
+    id   serial constraint artists_pk primary key,
+    name varchar(100) not null
+);
+            ",
+        ];
+    }
+
+    protected function getSqlSqlsrv(): array
+    {
+        return [];
+    }
+}
---
+++ tests/support/Migrations/SelectMigration.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Support\Migrations;
+
+/**
+ * Class SelectMigration
+ */
+class SelectMigration extends AbstractMigration
+{
+    protected $table = 'ph_select';
+
+    /**
+     * @param string      $name
+     * @param string|null $text
+     */
+    public function insert(
+        string $name,
+        ?string $text = null
+    ): void {
+        $textVal = $text === null ? 'null' : "'{$text}'";
+        $sql     = <<<SQL
+insert into ph_select (sel_name, sel_text)
+values ('{$name}', {$textVal})
+SQL;
+
+        $this->connection->exec($sql);
+    }
+
+    protected function getSqlMysql(): array
+    {
+        return [
+            "
+drop table if exists `ph_select`;
+            ",
+            "
+create table `ph_select`
+(
+    `sel_id`   int(11) unsigned not null auto_increment,
+    `sel_name` varchar(16)      not null,
+    `sel_text` varchar(32)               default null,
+    primary key (`sel_id`)
+);
+            ",
+        ];
+    }
+
+    protected function getSqlSqlite(): array
+    {
+        return [
+            "
+drop table if exists ph_select;
+            ",
+            "
+create table ph_select
+(
+    sel_id   integer not null,
+    sel_name text    not null,
+    sel_text text             default null,
+    primary key (sel_id autoincrement)
+);
+            ",
+        ];
+    }
+
+    protected function getSqlPgsql(): array
+    {
+        return [
+            "
+drop table if exists ph_select;
+            ",
+            "
+create table ph_select
+(
+    sel_id   serial      not null constraint ph_select_pk primary key,
+    sel_name varchar(16) not null,
+    sel_text varchar(32)          default null
+);
+            ",
+        ];
+    }
+
+    protected function getSqlSqlsrv(): array
+    {
+        return [];
+    }
+}
+
