643e713520766279cac761e1dd936b9ae467458c
Nikolaos Dimopoulos <nikos@niden.net>
2026-05-06 19:04:29 -0500

    [#16273] - adding exception

---
+++ src/Auth/Exception.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ *
+ * Implementation of this file has been influenced by sinbadxiii/cphalcon-auth
+ * @link    https://github.com/sinbadxiii/cphalcon-auth
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Auth;
+
+/**
+ * Phalcon\Auth\Exception
+ *
+ * Exceptions thrown in Phalcon\Auth will use this class
+ */
+class Exception extends \Exception
+{
+}
