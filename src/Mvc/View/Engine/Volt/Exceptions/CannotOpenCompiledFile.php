46eb4c79cdba9f21234482a5bd21fbec770c80d7
Nikolaos Dimopoulos <nikos@niden.net>
2026-05-29 09:23:07 -0500

    [#6.0.x] - adjustments;typos; types

---
+++ src/Mvc/View/Engine/Volt/Exceptions/CannotOpenCompiledFile.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Mvc\View\Engine\Volt\Exceptions;
+
+use Phalcon\Mvc\View\Engine\Volt\Exception;
+
+class CannotOpenCompiledFile extends Exception
+{
+    public function __construct(string $path)
+    {
+        parent::__construct(
+            "Extends compilation file " . $path . " could not be opened"
+        );
+    }
+}
---
+++ src/Mvc/View/Exceptions/SimpleViewNotFound.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Mvc\View\Exceptions;
+
+use Phalcon\Mvc\View\Exception;
+
+class SimpleViewNotFound extends Exception
+{
+    public function __construct(string $viewsDirPath)
+    {
+        parent::__construct(
+            "View '" . $viewsDirPath . "' was not found in the views directory"
+        );
+    }
+}
