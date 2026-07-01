5d586d90d92db78f910c03704550a625db6456e2
Nikolaos Dimopoulos <nikos@niden.net>
2026-06-30 22:32:00 -0500

    [#769] - migrate tests/unit/Encryption

---
+++ tests/unit/Encryption/Security/JWT/Validator/ValidateIdTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Encryption\Security\JWT\Validator;
+
+use Phalcon\Encryption\Security\JWT\Validator;
+use Phalcon\Talon\PHPUnit\AbstractUnitTestCase;
+use Phalcon\Tests\Unit\Encryption\Fake\JWTTrait;
+
+final class ValidateIdTest extends AbstractUnitTestCase
+{
+    use JWTTrait;
+
+    /**
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-09-09
+     */
+    public function testEncryptionSecurityJWTValidatorValidateId(): void
+    {
+        $token     = $this->newToken();
+        $validator = new Validator($token);
+        $this->assertInstanceOf(Validator::class, $validator);
+
+        $validator->validateId("unknown");
+
+        $expected = ["Validation: incorrect Id"];
+        $actual   = $validator->getErrors();
+        $this->assertSame($expected, $actual);
+    }
+}
