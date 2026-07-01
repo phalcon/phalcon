44125bc350267986dd5ac1bd425c9eaa7d6b43e8
Nikolaos Dimopoulos <nikos@niden.net>
2026-06-30 22:16:39 -0500

    [#769] - migrate tests/unit/Translate

---
+++ tests/unit/Translate/Interpolator/IndexedArray/ReplacePlaceholdersTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Translate\Interpolator\IndexedArray;
+
+use Phalcon\Talon\PHPUnit\AbstractUnitTestCase;
+use Phalcon\Translate\Interpolator\IndexedArray;
+
+final class ReplacePlaceholdersTest extends AbstractUnitTestCase
+{
+    /**
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-09-09
+     */
+    public function testTranslateInterpolatorIndexedarrayReplacePlaceholders(): void
+    {
+
+        $interpolator = new IndexedArray();
+
+        $actual = $interpolator->replacePlaceholders('Hello, %s %s %s!', ['John',
+                                                                          'D.',
+                                                                          'Doe',]);
+
+        $this->assertSame('Hello, John D. Doe!', $actual);
+    }
+
+    /**
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-09-09
+     */
+    public function testTranslateInterpolatorIndexedarrayReplacePlaceholdersWithNoPlaceholders(): void
+    {
+
+        $interpolator = new IndexedArray();
+
+        $source   = 'Hello, %s %s %s!';
+        $expected = $source;
+        $actual   = $interpolator->replacePlaceholders('Hello, %s %s %s!', []);
+        $this->assertSame($expected, $actual);
+    }
+}
